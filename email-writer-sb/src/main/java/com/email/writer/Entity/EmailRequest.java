750f69f4d3448327c7bbad1c744b86ef009fb7f1
Pratheesh M Gowda <pratheeshmgowda01@gmail.com>
2025-09-03 20:01:03 +0530

    Initial commit with backend and frontend code

---
+++ email-writer-sb/src/main/java/com/email/writer/Entity/EmailRequest.java
@@ -0,0 +1,25 @@
+package com.email.writer.Entity;
+
+public class EmailRequest {
+
+    private String emailContent;
+    private String tone;
+
+    public EmailRequest() {}
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+
+    public void setEmailContent(String emailContent) {
+        this.emailContent = emailContent;
+    }
+
+    public String getTone() {
+        return tone;
+    }
+
+    public void setTone(String tone) {
+        this.tone = tone;
+    }
+}
